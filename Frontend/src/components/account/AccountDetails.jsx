fcd94ed32798d25e6ad5c29d94d1d32ad6f3abe1
ashutoshsingh2073 <145258929+ashutoshsingh2073@users.noreply.github.com>
2024-08-16 21:42:02 +0530

    Update AccountDetails.jsx

--- Frontend/src/components/account/AccountDetails.jsx
+++ Frontend/src/components/account/AccountDetails.jsx
@@ -33,9 +33,9 @@ export const AccountDetails = ({ account }) => {
           Balance
         </p>
         <span className="w-full lg:w-auto text-slate-900 bg-white px-4 py-2 rounded-md">
-          {new Intl.NumberFormat("ar-EG", {
+          {new Intl.NumberFormat("en-IN", {
             style: "currency",
-            currency: "EGP",
+            currency: "INR",
           }).format(account.balance)}
         </span>
       </div>
@@ -46,19 +46,19 @@ export const AccountDetails = ({ account }) => {
         </p>
         <span className="w-full lg:w-auto text-slate-900 bg-white  px-4 py-2 rounded-md">
           {account.out.length > 0
-            ? new Intl.NumberFormat("ar-EG", {
-                style: "currency",
-                currency: "EGP",
-              }).format(
+            ? new Intl.NumberFormat("en-IN", {
+            style: "currency",
+            currency: "INR",
+          }).format(
                 account.out.reduce(
                   (totalAmount, log) => (totalAmount += log.balance_transfered),
                   0
                 )
               )
-            : new Intl.NumberFormat("ar-EG", {
-                style: "currency",
-                currency: "EGP",
-              }).format(0)}
+            : new Intl.NumberFormat("en-IN", {
+            style: "currency",
+            currency: "INR",
+          }).format(0)}
         </span>
       </div>
 
@@ -68,19 +68,19 @@ export const AccountDetails = ({ account }) => {
         </p>
         <span className="w-full lg:w-auto text-slate-900 bg-white  px-4 py-2 rounded-md">
           {account.in.length > 0
-            ? new Intl.NumberFormat("ar-EG", {
-                style: "currency",
-                currency: "EGP",
-              }).format(
+            ? new Intl.NumberFormat("en-IN", {
+            style: "currency",
+            currency: "INR",
+            }).format(
                 account.in.reduce(
                   (totalAmount, log) => (totalAmount += log.balance_transfered),
                   0
                 )
               )
-            : new Intl.NumberFormat("ar-EG", {
-                style: "currency",
-                currency: "EGP",
-              }).format(0)}
+            : new Intl.NumberFormat("en-IN", {
+            style: "currency",
+            currency: "INR",
+          }).format(0)}
         </span>
       </div>
 
@@ -90,19 +90,19 @@ export const AccountDetails = ({ account }) => {
         </p>
         <span className="w-full lg:w-auto text-slate-900 bg-white  px-4 py-2 rounded-md">
           {account.deposit_logs.length > 0
-            ? new Intl.NumberFormat("ar-EG", {
-                style: "currency",
-                currency: "EGP",
-              }).format(
+            ? new Intl.NumberFormat("en-IN", {
+            style: "currency",
+            currency: "INR",
+             }).format(
                 account.deposit_logs.reduce(
                   (totalAmount, log) => (totalAmount += log.depositted_amount),
                   0
                 )
               )
-            : new Intl.NumberFormat("ar-EG", {
-                style: "currency",
-                currency: "EGP",
-              }).format(0)}
+            : new Intl.NumberFormat("en-IN", {
+            style: "currency",
+            currency: "INR",
+             }).format(0)}
         </span>
       </div>
 
@@ -112,19 +112,19 @@ export const AccountDetails = ({ account }) => {
         </p>
         <span className="w-full lg:w-auto text-slate-900 bg-white  px-4 py-2 rounded-md">
           {account.withdraw_logs.length > 0
-            ? new Intl.NumberFormat("ar-EG", {
-                style: "currency",
-                currency: "EGP",
+            ? new Intl.NumberFormat("en-IN", {
+              style: "currency",
+              currency: "INR",
               }).format(
                 account.withdraw_logs.reduce(
                   (totalAmount, log) => (totalAmount += log.withdrawed_amount),
                   0
                 )
               )
-            : new Intl.NumberFormat("ar-EG", {
-                style: "currency",
-                currency: "EGP",
-              }).format(0)}
+            :new Intl.NumberFormat("en-IN", {
+            style: "currency",
+            currency: "INR",
+           }).format(0)}
         </span>
       </div>
     </div>
